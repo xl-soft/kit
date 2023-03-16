584a74606b855511008f6402261395d86224b875
TypoStudio <typostudio48@gmail.com>
2023-03-16 16:14:48 +0300

    feat: demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,61 @@
+<script>
+</script>
 
-<h1>Welcome</h1>
+<main>
+    <a href="https://github.com/xl-soft/kit">
+        <img src="https://psv4.userapi.com/c235031/u539876810/docs/d32/33bf7de6fab1/Group_4.png?extra=AmmuvsPM4kjAnxZ0q-71XFsdaeBqng4LhgMi_qvmb4zqrc2r4L324ln4wvfhNytQ1gClkyv5CnuLFgOLwEMJTmdVuuGqVS2TbvaRzuo_qdrdxWK_AyVIrt33vO3QKuud5BErrs7VL-yy3tNji3pyfV3_QwM" alt="">
+    </a>
+    <h2>
+        <span style="width: 400px">I can't think of an acronym,</span><br><span>but in Russian it's a <a class="whale" href="https://translate.google.ru/?sl=en&tl=ru&text=whale&op=translate" style="color:dodgerblue">whale</a></span> </h2>
+</main>
+
+<style>
+    .whale:hover {
+        opacity: .5;
+    }
+    .whale {
+        width: 400px;
+        cursor: pointer;
+        text-decoration: none;
+    }
+    img:hover {
+        width: 165px;
+        opacity: .5;
+    }
+    span {
+        text-align: center
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap');
+    
+    h2 {
+        color: white;
+        font-family: "Dela Gothic One";
+        text-align: center;
+    }
+
+    img {
+        width: 150px;
+        cursor: pointer;
+    }
+
+    main {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 15px;
+    }
+
+    :global(body) {
+        margin: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgb(0, 11, 36);
+    }
+    * {
+        transition: .4s;
+    }
+</style>
